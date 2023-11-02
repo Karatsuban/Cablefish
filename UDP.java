d9e737dcb3873b3971982f714a1df631e37c63a6
Karatsuban <raphael.garnier1919@gmail.com>
2023-11-02 02:53:35 +0100

    Parsed UDP, checked if DHCP is encapsulated

--- UDP.java
+++ UDP.java
@@ -2,6 +2,12 @@
 
 class UDP extends Protocol{
 
+	private int sourcePort;
+	private int destPort;
+	private int length;
+	private ByteUtil payload;
+
+
 	public UDP(ByteUtil data){
 		super("UDP", data);
 
@@ -10,14 +16,39 @@ class UDP extends Protocol{
 
 
 	private void parseData(){
-		//System.out.println("Parsing UDP...");
+		this.sourcePort = this.data.readBytes(2).toInt();
+		this.destPort = this.data.readBytes(2).toInt();
+		this.length = this.data.readBytes(2).toInt(); // in bytes !
+		this.data.skipBytes(2); // skip the checksum value
+
+		if (this.length-2 > 0){
+			this.payload = this.data.getRemainingBytes();
+
+			if (this.payload.length >= 240){
+				this.payload.skipBytes(236);
+				boolean isDHCP = this.payload.getBytes(4).equals("63825363");
+				this.payload.reset();
+				if (isDHCP){
+					this.setEncapsulated(new DHCP(this.payload));
+				}
+			}
+		}
 	}
 
 
 	public String toString(){
 		String out = "";
 		out += this.gs()+this.protocolName+"\n";
-		out += this.gs()+"data: "+this.data;
+		out += this.gs()+"Source port: "+this.sourcePort+"\n";
+		out += this.gs()+"Destination port: "+this.destPort+"\n";
+		if (this.payload != null){
+			out += this.gs()+"len payload: "+this.payload.length+"\n";
+		}
+
+		if (this.encapsulated != null){
+			out += this.gs()+"Encapsulated protocol:\n";
+			out += this.gs()+this.encapsulated.toString(this.indent+1);
+		}
 		out += "\n";
 		return out;
 	}
